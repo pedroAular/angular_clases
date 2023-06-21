4705860a19c2c89dfdb2df303ae30d836ffc47e2
pedro aular <pedroeliasaularmedina@gmail.com>
2023-06-20 23:22:12 -0400

    primer entregable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+import { MyPComponent } from './components/my-p/my-p.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolbarComponent,
+    ContenidoComponent,
+    MyPComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/my-p/my-p.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-p',
+  template: `
+<main>
+
+
+<ng-content></ng-content>
+</main>
+  `,
+
+
+  styles: [
+  ]
+})
+export class MyPComponent {
+
+}
